1c0907482fe95c33f8048dd420f5abac188f4ad6
shining013 <nevertheless113@naver.com>
2024-12-05 11:44:00 +0900

    fix: delete

--- app/src/main/java/com/autoever/carmanager4/activities/MainActivity.kt
+++ app/src/main/java/com/autoever/carmanager4/activities/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.autoever.carmanager4.activities
 
-import android.location.Location
 import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import android.Manifest
@@ -20,38 +14,12 @@ import com.autoever.carmanager4.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var manager: LocationManager
     private lateinit var listener: LocationListener
     private lateinit var locationTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         enableEdgeToEdge()
-
-        //TextView 참조
-        locationTextView = findViewById(R.id.locationTextView)
-
-        // 위치 매니저 불러오기
-        manager = getSystemService(LOCATION_SERVICE) as LocationManager
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        // 위치 리스너
-        listener = object : LocationListener {
-            override fun onLocationChanged(location: Location) {
-                val locationInfo = "위도: ${location.latitude}, 경도:${location.longitude},정확도: ${location.accuracy}m"
-                Log.d("location", locationInfo)
-                locationTextView.text = locationInfo
-            }
-
-            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
-            override fun onProviderEnabled(provider: String) {}
-            override fun onProviderDisabled(provider: String) {}
-        }
-
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -71,12 +39,6 @@ class MainActivity : AppCompatActivity() {
             )
             return
         }
-        manager.requestLocationUpdates(
-            LocationManager.GPS_PROVIDER,
-            3000,
-            -1f,
-            listener
-        )
         // NavHostFragment 찾기
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
@@ -85,33 +47,5 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         bottomNavigationView.setupWithNavController(navController)
 
-    }
-
-    override fun onRequestPermissionsResult(
-      requestCode: Int,
-      permissions: Array<out String>,
-      grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 100) {
-            if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED ||
-                ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                finish()
-                return
-            }
-            manager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER,
-                3000,
-                -1f,
-                listener
-            )
-        }
     }
 }
